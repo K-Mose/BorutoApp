a6375fdc48eb9185ae398b36e47885634f125cc9
Mose <km_413@naver.com>
2022-12-05 16:20:39 +0900

    Rotate the image

--- app/src/main/java/com/mose/kim/borutoapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/mose/kim/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -1,6 +1,8 @@
 package com.mose.kim.borutoapp.presentation.screens.splash
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import com.mose.kim.borutoapp.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,8 +10,11 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -21,11 +26,31 @@ import com.mose.kim.borutoapp.ui.theme.Purple700
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+
+    val degrees = remember {
+        // androidx.compose.animation.core.Animatable O
+        // androidx.compose.animation.Animatable X
+        Animatable(0f)
+    }
+
+    // key1 = true : 1번만 실행
+    LaunchedEffect(key1 = true) {
+         // 실행될 행동 정의
+        degrees.animateTo(
+            targetValue = 360f,
+            // duration of animation
+            animationSpec = tween(
+                durationMillis = 1000, // 애니메이션이 작동되는 시간
+                delayMillis = 200 // 시작 전 기다리는 시간
+            )
+        )
+    }
+
+    Splash(degrees = degrees.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees: Float) {
     // 어두운 테마 여부
     if (isSystemInDarkTheme()) {
         Box(modifier = Modifier
@@ -33,7 +58,9 @@ fun Splash() {
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Image(painter = painterResource(id = R.drawable.ic_logo),
+            Image(
+                modifier = Modifier.rotate(degrees = degrees),
+                painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
         }
@@ -43,7 +70,9 @@ fun Splash() {
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Image(painter = painterResource(id = R.drawable.ic_logo),
+            Image(
+                modifier = Modifier.rotate(degrees = degrees),
+                painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
         }
@@ -53,13 +82,13 @@ fun Splash() {
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    Splash()
+    Splash(0f)
 }
 
 // 어두운 테마 Preview
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun SplashScreenDarkPreview() {
-    Splash()
+    Splash(0f)
 }
 
