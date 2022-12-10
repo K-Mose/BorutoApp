918088301371281f54b88c7dacc862b8f6eaf473
Mose <km_413@naver.com>
2022-12-10 13:13:11 +0900

    Design TopBar

--- app/src/main/java/com/mose/kim/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/mose/kim/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,17 @@
 package com.mose.kim.borutoapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
+    Scaffold(
+        topBar = {
+            HomeTopBar (onSearchClicked = {})
+        }
+    ) {
+        it
+    }
 }
