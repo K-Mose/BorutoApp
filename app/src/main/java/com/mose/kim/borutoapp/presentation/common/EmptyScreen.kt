c66fda6ad4d56fc69fe0831619fc0ca49e631832
Mose <km_413@naver.com>
2023-05-13 23:41:49 +0900

    Apply Animate & Separate Empty Content

--- app/src/main/java/com/mose/kim/borutoapp/presentation/common/EmptyScreen.kt
+++ app/src/main/java/com/mose/kim/borutoapp/presentation/common/EmptyScreen.kt
@@ -1,16 +1,18 @@
 package com.mose.kim.borutoapp.presentation.common
 
+import android.content.res.Configuration
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -36,21 +38,50 @@ fun EmptyScreen (
     val icon by remember {
         mutableStateOf(R.drawable.network_error)
     }
+    // animation
+    var startAnimation by remember {
+        mutableStateOf(false)
+    }
+    val alphaAnim by animateFloatAsState(
+        targetValue = if (startAnimation) ContentAlpha.disabled else 0f,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+    }
+    EmptyContent(
+        alphaAnim = alphaAnim,
+        icon = icon,
+        message = message
+    )
+}
 
+@Composable
+fun EmptyContent(
+    alphaAnim: Float,
+    icon: Int,
+    message: String
+) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
-            modifier = Modifier.size(NETWORK_ERROR_ICON_HEIGHT),
+            modifier = Modifier
+                .size(NETWORK_ERROR_ICON_HEIGHT)
+                .alpha(alpha = alphaAnim),
             painter = painterResource(id = icon),
             contentDescription = stringResource(R.string.network_error_icon),
             tint = if(isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
         )
         Text(
             text = message,
-            modifier = Modifier.padding(all = SMALL_PADDING) ,
+            modifier = Modifier
+                .padding(all = SMALL_PADDING)
+                .alpha(alpha = alphaAnim),
             color = if(isSystemInDarkTheme()) Color.LightGray else Color.DarkGray,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Medium,
@@ -72,11 +103,11 @@ fun parseErrorMessage(message: String): String = when {
 @Composable
 @Preview(showBackground = true)
 fun EmptyScreenPreview1() {
-    EmptyScreen(error = LoadState.Error(SocketTimeoutException()))
+    EmptyContent(1f, R.drawable.network_error, "SocketTimeoutException")
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 fun EmptyScreenPreview2() {
-    EmptyScreen(error = LoadState.Error(ConnectException()))
+    EmptyContent(1f, R.drawable.network_error, "ConnectException")
 }
